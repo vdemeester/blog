068602c2fee7401d732e58715023ee527159310e
Vincent Demeester <vincent@sbr.pm>
2015-05-03 21:07:36 +0200

    Initial migration to hugo

--- Gemfile
+++
@@ -1,7 +0,0 @@
-LANG="en_US.UTF-8"
-LC_ALL="en_US.UTF-8"
-
-source "http://rubygems.org"
-
-gem 'github-pages'
-
--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-docker build -t vdemeester-site .
-docker run -ti --rm -v $PWD:/usr/src/app -p 4000:4000 vdemeester-site
