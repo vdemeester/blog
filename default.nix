7216a06e722e4f5824a3ffb1ddb2b57d878ae402
Vincent Demeester <vincent@sbr.pm>
2018-10-07 18:18:14 +0200

    scripts: no need for direnv stuff…
    Use `make.nix` directly ;)
    
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- default.nix
+++
@@ -1,7 +0,0 @@
-with import <nixpkgs> {};
-stdenv.mkDerivation rec {
-  name = "vdf-blog";
-  buildInputs = [
-    pkgs.hugo
-  ];
-}
--- make.nix
+++ make.nix
@@ -4,7 +4,7 @@ with nixpkgs;
 
 rec {
   server = pkgs.writeShellScriptBin "script.sh" ''
-    CMD="hugo server --buildFuture --buildDrafts --bind=0.0.0.0"
+    CMD="${pkgs.hugo}/bin/hugo server --buildFuture --buildDrafts --bind=0.0.0.0"
     if test -n "$TMUX"; then
       tmux split-pane -p15 $CMD
     else
