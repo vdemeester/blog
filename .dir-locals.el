94af5062596fe740c8202540e527badbd1f0d4b1
Vincent Demeester <vincent@sbr.pm>
2019-01-18 16:19:34 +0100

    Update .dir-locals.el to add projectile run and compile commands
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,5 @@
-(("content-org/"
+((nil
+  (eval . (setq projectile-project-run-cmd "hugo serve --buildFuture --buildDrafts --bind=0.0.0.0"
+		projectile-project-compilation-cmd "hugo")))
+ ("content-org/"
   . ((org-mode . ((org-hugo-auto-export-on-save . t))))))
